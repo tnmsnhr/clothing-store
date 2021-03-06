efec65b9fa99eb2789879b97e25517a2be4b1fca
Tanmoy Singha Roy <tnmsnhr@gmail.com>
2021-03-07 04:12:01 +0530

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
           <Route exact path='/' component={Homepage}/>
           <Route path='/shop' component={ShopPage}/>
