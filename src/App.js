3d0368d81d17d0bc3b569910103215c05fd8db5c
Tanmoy Singha Roy <tnmsnhr@gmail.com>
2021-03-07 04:00:13 +0530

    creating shopPage, collectionPreview and collectionItem page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+          <Route exact path='/' component={Homepage}/>
+          <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
