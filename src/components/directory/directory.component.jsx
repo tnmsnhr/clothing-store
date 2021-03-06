72268ba6bf1994f5eeb5ddc175811da9865266bc
Tanmoy Singha Roy <tnmsnhr@gmail.com>
2021-03-07 03:14:51 +0530

    update menu-items with router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,7 +45,7 @@ export class Directory extends Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.section.map(({title,imageUrl,id,size})=><MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>)}
+                {this.state.section.map(({id, ...sectionProps})=><MenuItem key={id} {...sectionProps}/>)}
             </div>
         )
     }
