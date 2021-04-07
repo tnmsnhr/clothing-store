481f8af2e2113b4028b2dc252422c303698c6d72
Tanmoy Singha Roy <tnmsnhr@gmail.com>
2021-04-08 04:41:23 +0530

    adding selectors for cartItemCount and total item

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
-import './cart-dropdown.styles.scss'
+import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropdown = () => {
+const CartDropdown = ({cartItems}) => {
     return (
         <div className="cart-dropdown">
             <div className="cart-items">
-                <CustomButton>GO TO CHECKOUT</CustomButton>
+                {cartItems.map(cartItem=>(
+                    <CartItem key={cartItem.id} item={cartItem}/>
+                ))}
             </div>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 }
 
-export default CartDropdown;
+const mapStateToProps = state=>{
+    return {
+        cartItems: selectCartItems(state)
+    }
+}
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import {ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 const CartIcon = props => {
     return (
@@ -15,7 +16,7 @@ const CartIcon = props => {
 
 const mapStateToProps = state=>{
     return {
-        itemCount: state.cart.cartItems.flat(2).length
+        itemCount: selectCartItemsCount(state)
     }
 }
 
