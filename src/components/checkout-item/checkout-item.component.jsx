2ac4a45498f625a1309c044cc4392f7cfbcca699
Tanmoy Singha Roy <tnmsnhr@gmail.com>
2021-04-12 03:30:16 +0530

    added decrease and increase quantity feature and removeItem from cart feature

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,18 +1,32 @@
 import './checkout-item.styles.scss';
 import React from 'react'
+import { connect } from 'react-redux';
+import { addItem, clearItemFromCart, removeItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({cartItem:{name,imageUrl,price,quantity}}) => {
+const CheckoutItem = ({cartItem, clearItemFromCart, addItemToCart, removeItemFromCart}) => {
+
+    const {name,imageUrl,price,quantity}=cartItem;
     return (
         <div className="checkout-item">
             <div className="image-container">
                 <img alt='item' src={imageUrl}/>
             </div>
             <span className="name">{name}</span>
-            <span className="quantity">{quantity}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={()=>removeItemFromCart(cartItem)}>&#10094;</div>
+                    <span className="value">{quantity}</span>
+                <div className="arrow" onClick={()=>addItemToCart(cartItem)}>&#10095;</div>
+            </span>
             <span className="price">{price}</span>
-            <div className="remove-button">&#10006;</div>
+            <div className="remove-button" onClick={()=>clearItemFromCart(cartItem)}>&#10006;</div>
         </div>
     )
 }
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch=>({
+    clearItemFromCart: item=>dispatch(clearItemFromCart(item)),
+    addItemToCart: item=>dispatch(addItem(item)),
+    removeItemFromCart: item=>dispatch(removeItem(item))
+})
+
+export default connect(null,mapDispatchToProps)(CheckoutItem);
